6505e0c42f52018e73ff6d3fb62b010da73a1406
jerrylammel <68579815+jerrylammel@users.noreply.github.com>
2022-12-15 23:45:33 +1100

    functions enable adding a product into the cart

--- js/cart.js
+++ js/cart.js
@@ -12,12 +12,53 @@ class CartManager {
 
     }
 
-    addProduct (product) {
+    calculateTotolPrice() {
+        let sum = 0;
+        this._products.forEach((product) => {
+            sum += product.total;
+        });
 
+        return sum;
+    }
+
+    addProduct (product) {
+        this._products.push(product);
+        const productStr = JSON.stringify(product);
+        localStorage.setItem(`${product.id}`, productStr);
     }
 
     addProductHTML(product) {
-    
+        const productDiv = document.createElement('div');
+        productDiv.setAttribute('id', `${product.id}-item`);
+        productDiv.setAttribute('class', 'row');
+
+        productDiv.innerHTML = `<div class="col-4 border border-top-0 border-start-1 border-bottom-1 border-end-0 border-secondary">
+                            ${product.name}
+                            </div>
+                            <div class="col-2 border border-top-0 border-start-1 border-bottom-1 border-end-0 border-secondary">
+                            ${product.quantity}
+                            </div>
+                            <div class="col-2 border border-top-0 border-start-1 border-bottom-1 border-end-0 border-secondary">
+                            $${product.price}
+                            </div>
+                            <div class="col-2 border border-top-0 border-start-1 border-bottom-1 border-end-0 border-secondary">
+                            $${product.total}
+                            </div>
+                            <div class="col-2 border border-top-0 border-start-1 border-bottom-1 border-end-1 border-secondary">
+                                <button class="btn-remove" id="${product.id}-remove">Remove</button>
+                            </div>`;
+
+        return productDiv;
+    }
+
+    renderProduct (productDiv) {
+        const shoppingList = document.getElementById("shopping-list");
+        const tableTotal = document.getElementById("table-total");
+        shoppingList.insertBefore(productDiv, tableTotal);
+        // update total price all items
+        const totalPrice = this.calculateTotolPrice();
+        const priceDiv = document.getElementById('total-price');
+        priceDiv.innerHTML = `$ ${totalPrice}`;
     }
 
     removeProduct(productId) {
@@ -29,10 +70,6 @@ class CartManager {
     }
 
     loadStoredProducts() {
-
-    }
-
-    renderProduct (taskHTML) {
 
     }
 
--- js/script.js
+++ js/script.js
@@ -1,2 +1,37 @@
-import { CartManager } from './cart';
+import { CartManager } from './cart.js';
 
+const createProductObj = (id) => {
+    return {
+        id: id,
+        imgSrc: "",
+        name: "",
+        price: 0,
+        quantity: 0,
+        total: 0
+    }
+};
+
+window.addToCart = function (event) {
+    event.preventDefault();
+    // get digimark02 from id digimark02-addtocart
+    const productId = event.target.id.split('-')[0];
+    const productObj = createProductObj(productId);
+    const imgSrc = document.querySelector(`#${productId} img`).getAttribute('src');
+    const productName = document.querySelector(`#${productId} .card-title`).innerText;
+    const productPrice = document.querySelector(`#${productId} .card-subtitle`).getAttribute('id');
+    const quantity = document.getElementById(`${productId}-quantity`).value;
+
+    productObj.id = productId;
+    productObj.imgSrc = imgSrc;
+    productObj.name = productName;
+    productObj.price = productPrice;
+    productObj.quantity = quantity;
+    productObj.total = productPrice * quantity;
+
+    myCart.addProduct(productObj);
+    const productHTML = myCart.addProductHTML(productObj);
+    myCart.renderProduct(productHTML);
+}
+
+
+const myCart = new CartManager('digiMart');

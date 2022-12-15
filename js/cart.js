edc5ab0d4033109640fb1006b009c53e33bcf49e
jerrylammel <68579815+jerrylammel@users.noreply.github.com>
2022-12-15 12:53:05 +1100

    add initial files

---
+++ js/cart.js
@@ -0,0 +1,44 @@
+class CartManager {
+    constructor(user) {
+        this._user = user;
+        this._products = [];
+    }
+
+    get getAllProducts () {
+        return this._products;
+    }
+
+    getProduct (id) {
+
+    }
+
+    addProduct (product) {
+
+    }
+
+    addProductHTML(product) {
+    
+    }
+
+    removeProduct(productId) {
+
+    }
+
+    removepProductHtml(productId) {
+
+    }
+
+    loadStoredProducts() {
+
+    }
+
+    renderProduct (taskHTML) {
+
+    }
+
+    addAllProductsFromLocalStorage (allProducts) {
+    }
+    
+}
+
+export { CartManager };
---
+++ js/script.js
@@ -0,0 +1,2 @@
+import { CartManager } from './cart';
+
